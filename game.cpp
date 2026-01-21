c25e60abba1a2808ee0725c360e856a346802072
Ander <157061485+Brenninho123@users.noreply.github.com>
2026-01-20 22:12:39 -0300

    Atualizar o game.cpp

--- game.cpp
+++ game.cpp
@@ -1,34 +1,28 @@
-
 #include <emscripten.h>
 #include <cstdlib>
 #include <ctime>
 
 extern "C" {
 
-// Inicializa semente aleatória
 EMSCRIPTEN_KEEPALIVE
 void initRandom() {
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
 }
 
-// Função para adicionar ponto
 EMSCRIPTEN_KEEPALIVE
 int addPoint(int currentScore) {
     int bonus = 0;
 
     // 20% de chance de bônus de +2 pontos
     int chance = std::rand() % 100;
-    if (chance < 20) {
-        bonus = 2;
-    }
+    if (chance < 20) bonus = 2;
 
     return currentScore + 1 + bonus;
 }
 
-// Função para resetar pontos
 EMSCRIPTEN_KEEPALIVE
 int resetPoints() {
     return 0;
 }
 
-} // extern "C"
+}
